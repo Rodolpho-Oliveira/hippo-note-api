7ee8e8db3306ef547d6a0dd45a597ebb563a5ef9
Rodolpho Oliveira <j.rodolpho29@hotmail.com>
2023-10-03 03:43:46 -0300

    fix: change note schema object

--- src/schemas/noteSchema.ts
+++ src/schemas/noteSchema.ts
@@ -3,6 +3,6 @@ import Joi from "joi"
 export const noteSchema = Joi.object({
     title: Joi.string().min(1).max(30).required(),
     description: Joi.string().min(1).max(999).required(),
-    category: Joi.string().required(),
+    categoryName: Joi.string().required(),
     image: Joi.string().uri().allow('')
 })
