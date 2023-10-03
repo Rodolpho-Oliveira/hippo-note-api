e421946bdcc6b261c42bf7fdf9df5105e26ee4b3
Rodolpho Oliveira <j.rodolpho29@hotmail.com>
2023-10-03 05:21:32 -0300

    fix: add remove/update note to routes

--- src/routes/noteRouter.ts
+++ src/routes/noteRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import { verifyNote } from "../middlewares/noteMiddleware.js"
-import { createNewCategory, createNewNote, getAllCategories, getAllNotes, removeCategory } from "../controllers/noteController.js"
+import { createNewCategory, createNewNote, getAllCategories, getAllNotes, removeCategory, removeNote, updateNote } from "../controllers/noteController.js"
 
 const noteRouter = Router()
 
 noteRouter.post("/notes", verifyNote, createNewNote)
 noteRouter.get("/notes", getAllNotes)
+noteRouter.delete("/notes", removeNote)
+noteRouter.put("/notes", verifyNote, updateNote)
 noteRouter.post("/categories", createNewCategory)
 noteRouter.get("/categories", getAllCategories)
 noteRouter.delete("/categories", removeCategory)
