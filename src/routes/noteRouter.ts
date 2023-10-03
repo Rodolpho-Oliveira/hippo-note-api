f233e42994669795ae6c1d788e406e27516388bc
Rodolpho Oliveira <j.rodolpho29@hotmail.com>
2023-10-03 02:49:10 -0300

    fix: add categories controller to route

--- src/routes/noteRouter.ts
+++ src/routes/noteRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { verifyNote } from "../middlewares/noteMiddleware.js"
-import { createNewNote, getAllNotes } from "../controllers/noteController.js"
+import { createNewCategory, createNewNote, getAllNotes } from "../controllers/noteController.js"
 
 const noteRouter = Router()
 
 noteRouter.post("/notes", verifyNote, createNewNote)
 noteRouter.get("/notes", getAllNotes)
+noteRouter.post("/categories", createNewCategory)
 
 export default noteRouter
